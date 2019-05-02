f5685470765de0ed068dd2eaf6c22520df2f24c3
William Akinsanya <akinsanya.will@gmail.com>
2019-05-02 22:35:53 +0100

    use a k_set to clean huffman code when necessary, corrected empirical average length calculation

--- huffman_test.py
+++ huffman_test.py
@@ -10,6 +10,8 @@ x_pmf = {
 }
 
 x_entropy = 0
+k_set = []
+code_lengths = {}
 
 def resolve_huffman_input(ensemble, d):
     if not ((len(ensemble) - 1) / (d - 1) == int((len(ensemble) - 1) / (d - 1))):
@@ -17,6 +19,7 @@ def resolve_huffman_input(ensemble, d):
         i = 1
         while(not resolved):
             ensemble['k{}'.format(i)] = 0
+            k_set.append('k{}'.format(i))
             if (len(ensemble) - 1) / (d - 1) == int((len(ensemble) - 1) / (d - 1)):
                 resolved = True
                 return ensemble
@@ -35,6 +38,7 @@ with open('NC_000964.3.seq') as bsb_genome:
         x_pmf[key] = x_pmf[key] / dna_length
 
     print('X pmf = {}\n'.format(x_pmf))   
+    code_lengths['dna_length'] = dna_length
 
 # Encode for D = 2, 3, 4
 for i in range(2, 5):
@@ -42,10 +46,15 @@ for i in range(2, 5):
     huffman_code = huffman.huffman(resolve_huffman_input(x_pmf.copy(), i), i)
     print(huffman_code)
     with open('Encoded-D{}-NC_000964.3.seq'.format(i), 'w') as encoding:
+        code_length = 0
         with open('NC_000964.3.seq') as bsb_genome:
             for line in bsb_genome:
                   for c in line:
-                      encoding.write(str(huffman_code[c]))
+                      code = str(huffman_code[c])
+                      code_length += len(code)
+                      encoding.write(code)
+
+    code_lengths['d{}'.format(i)] = code_length     
     print('done\n')
 
 # decode huffman codes
@@ -59,7 +68,7 @@ for i in range(2, 5):
             for line in encoding:
                 for c in line:
                     buffer.append(c)
-                    if (''.join(buffer) in huffman_decoder.keys()):
+                    if (''.join(buffer) in huffman_decoder):
                         decoding.write(huffman_decoder[''.join(buffer)])
                         buffer = []
     print('done\n')
@@ -72,20 +81,23 @@ for i in range(2, 5):
     else:
         raise Exception('Incorrect decoding of huffman code for d ={}'.format(i))    
 
-# Compute entropies, empirical average length and average codeword lengths
+# Compute entropies, empirical average length    h and average codeword lengths
 for i in range(2, 5):
     for key in x_pmf:
          x_entropy += x_pmf[key] * (math.log(x_pmf[key]) / math.log(i))
 
     x_entropy = -1 * x_entropy    
-
     print('H(X) = {} for d = {}'.format(x_entropy, i))
     print('Calculating empirical average length of the huffman encoding for d = {}'.format(i))
     huffman_code = huffman.huffman(resolve_huffman_input(x_pmf.copy(), i), i)
-    empirical_average_length = len(''.join(huffman_code.values())) / i
+
+    for k in k_set:
+        if k in huffman_code.keys():
+             huffman_code.pop(k)
+
+    empirical_average_length = code_lengths['d{}'.format(i)] / code_lengths['dna_length']
     print('Empirical average length = {}\ndone'.format(empirical_average_length))
     print('Determining average codeword length for the huffman encoding for d = {}'.format(i))
     print('Average codeword length = {}'.format(len(''.join(huffman_code.values())) / len(huffman_code)))
     print('done\n')
     x_entropy = 0
-  
